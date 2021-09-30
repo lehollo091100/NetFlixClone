92cc04316284976d4da0ea8018ef675fba14fd29
thien <lehollo09112000@gmail.com>
2021-09-30 16:16:31 +0700

    fix-overflow-item

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -81,6 +81,72 @@ function Home() {
             <div className="home__slider">
                 <a href="" className="home__slider-header">
                     <span>
+                        Hiện đang hiện hành
+                        <span className="watch__all">
+                            <span>Xem tất cả</span>
+                            <FontAwesomeIcon icon="chevron-right" className="home__slider-arrowright"></FontAwesomeIcon>
+                        </span>
+                    </span>
+                </a>
+                <Slider {...settings}>
+                    {
+                        movieList&&movieList.map(movie=>(
+                        <div className="home__slider-item">
+                            <img className="home__slider-item-img" src={movie? baseUrl.concat(movie.poster_path):''} alt="" />
+                            <div className="home__slider-detail">
+                                <div className="home__item-img">
+                                    <img src={movie? baseUrl.concat(movie.poster_path):''} alt="" />
+                                    <div className="mute__icon" onClick={handleMuteItem}>
+                                        <FontAwesomeIcon className="iconvolumn" icon="volume-mute"></FontAwesomeIcon>
+                                        <FontAwesomeIcon className="iconvolumn" icon="volume-up"></FontAwesomeIcon>
+                                    </div>
+                                </div>
+                                <div className="detail__item-button">
+                                    <div className="item__button-left">
+                                        <span>
+                                            <FontAwesomeIcon className="icon" icon="play"></FontAwesomeIcon>
+                                        </span><span>
+                                            <FontAwesomeIcon className="icon" icon="plus"></FontAwesomeIcon>
+                                        </span><span>
+                                            <FontAwesomeIcon className="icon" icon="thumbs-up"></FontAwesomeIcon>
+                                        </span><span>
+                                            <FontAwesomeIcon className="icon" icon="thumbs-down"></FontAwesomeIcon>
+                                        </span>
+                                    </div>
+                                    <div className="item__button-right">
+                                        <span>
+                                            <FontAwesomeIcon className="icon" icon="chevron-down"></FontAwesomeIcon>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="detail__item-info">
+                                    <span className="item__similarity">
+                                        Độ trùng : 98%
+                                    </span>
+                                    <span className="item__age">
+                                        18+
+                                    </span>
+                                    <span className="iem__season">
+                                        3 Mùa
+                                    </span>
+                                    <span className="item__standard">HD</span>
+                                </div>
+                                <div className="detail__item-categories">
+                                    <span>Bạo lực-</span>
+                                    <span>Thời kì lịch sử-</span>
+                                    <span>Chính kịch</span>
+                                </div>
+                            </div>
+                        </div>
+                        ))
+                    }
+                    
+                    
+                </Slider>
+            </div>
+            <div className="home__slider">
+                <a href="" className="home__slider-header">
+                    <span>
                         Top 10 tại Việt Nam hôm nay
                         <span className="watch__all">
                             <span>Xem tất cả</span>
@@ -89,102 +155,59 @@ function Home() {
                     </span>
                 </a>
                 <Slider {...settings}>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[0].poster_path):''} alt="" />
-                        <div className="home__slider-detail">
-                            <div className="home__item-img">
-                                <img src={movieList[0]? baseUrl.concat(movieList[0].poster_path):''} alt="" />
-                                <div className="mute__icon" onClick={handleMuteItem}>
-                                    <FontAwesomeIcon className="iconvolumn" icon="volume-mute"></FontAwesomeIcon>
-                                    <FontAwesomeIcon className="iconvolumn" icon="volume-up"></FontAwesomeIcon>
+                    {
+                        movieList&&movieList.map(movie=>(
+                        <div className="home__slider-item">
+                            <img className="home__slider-item-img" src={movie? baseUrl.concat(movie.poster_path):''} alt="" />
+                            <div className="home__slider-detail">
+                                <div className="home__item-img">
+                                    <img src={movie? baseUrl.concat(movie.poster_path):''} alt="" />
+                                    <div className="mute__icon" onClick={handleMuteItem}>
+                                        <FontAwesomeIcon className="iconvolumn" icon="volume-mute"></FontAwesomeIcon>
+                                        <FontAwesomeIcon className="iconvolumn" icon="volume-up"></FontAwesomeIcon>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="detail__item-button">
-                                <div className="item__button-left">
-                                    <span>
-                                        <FontAwesomeIcon className="icon" icon="play"></FontAwesomeIcon>
-                                    </span><span>
-                                        <FontAwesomeIcon className="icon" icon="plus"></FontAwesomeIcon>
-                                    </span><span>
-                                        <FontAwesomeIcon className="icon" icon="thumbs-up"></FontAwesomeIcon>
-                                    </span><span>
-                                        <FontAwesomeIcon className="icon" icon="thumbs-down"></FontAwesomeIcon>
+                                <div className="detail__item-button">
+                                    <div className="item__button-left">
+                                        <span>
+                                            <FontAwesomeIcon className="icon" icon="play"></FontAwesomeIcon>
+                                        </span><span>
+                                            <FontAwesomeIcon className="icon" icon="plus"></FontAwesomeIcon>
+                                        </span><span>
+                                            <FontAwesomeIcon className="icon" icon="thumbs-up"></FontAwesomeIcon>
+                                        </span><span>
+                                            <FontAwesomeIcon className="icon" icon="thumbs-down"></FontAwesomeIcon>
+                                        </span>
+                                    </div>
+                                    <div className="item__button-right">
+                                        <span>
+                                            <FontAwesomeIcon className="icon" icon="chevron-down"></FontAwesomeIcon>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="detail__item-info">
+                                    <span className="item__similarity">
+                                        Độ trùng : 98%
                                     </span>
-                                </div>
-                                <div className="item__button-right">
-                                    <span>
-                                        <FontAwesomeIcon className="icon" icon="chevron-down"></FontAwesomeIcon>
+                                    <span className="item__age">
+                                        18+
                                     </span>
+                                    <span className="iem__season">
+                                        3 Mùa
+                                    </span>
+                                    <span className="item__standard">HD</span>
                                 </div>
-                            </div>
-                            <div className="detail__item-info">
-                                <span className="item__similarity">
-                                    Độ trùng : 98%
-                                </span>
-                                <span className="item__age">
-                                    18+
-                                </span>
-                                <span className="iem__season">
-                                    3 Mùa
-                                </span>
-                                <span className="item__standard">HD</span>
-                            </div>
-                            <div className="detail__item-categories">
-                                <span>Bạo lực-</span>
-                                <span>Thời kì lịch sử-</span>
-                                <span>Chính kịch</span>
+                                <div className="detail__item-categories">
+                                    <span>Bạo lực-</span>
+                                    <span>Thời kì lịch sử-</span>
+                                    <span>Chính kịch</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[1].poster_path):''} alt="" />
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[2].poster_path):''} alt="" />
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[3].poster_path):''} alt="" />
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[4].poster_path):''} alt="" />
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[5].poster_path):''} alt="" />
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[6].poster_path):''} alt="" />
-                    </div>
-                    <div className="home__slider-item">
-                        <img className="home__slider-item-img" src={movieList[0]? baseUrl.concat(movieList[6].poster_path):''} alt="" />
-                    </div>
-                    
+                        ))
+                    }
                 </Slider>
             </div>
-            <div className="home__start">
-                <div className="home__start-wrapper">
-                    <img className="home__start-background" src="https://occ-0-325-395.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABc5MwMGTLgLIUHz4GoejmJxHbbebq628L2xETbeouq62894K4b2RewxTb2-2T2zN3u7CYHHss7rDZX40gEGY3QebyVFnybJJBpqq.webp?r=047" alt="" />
-                    <div className="home__start-info">
-                        Vòng xoáy dữ dội của những lời dối trá, sự phản bội và lòng thù hận xé tan cuộc hôn nhân tưởng chừng như hoàn hảo giữa một vị bác sĩ và một nhà làm phim.
-                    </div>
-                    <div className="home__start-button">
-                        <span className="play__button">
-                            <FontAwesomeIcon className="play__arrow" icon="caret-right"></FontAwesomeIcon>
-                            <span>Phát</span>
-                        </span>
-                        <span className="info__button">
-                            <FontAwesomeIcon className="info__icon" icon="info-circle"></FontAwesomeIcon>
-                            <span>Thông tin khác</span>
-                        </span>
-                    </div>
-                </div>
-                <div className="home__start-right">
-                    <span className="replay__button">
-                        <FontAwesomeIcon className="undo__icon" icon="undo-alt"></FontAwesomeIcon>
-                    </span>
-                    <span className="home__start-age">16+</span>
-                </div>
-            </div>
-
         </div>
     )
 }
